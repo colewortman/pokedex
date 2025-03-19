582a9401d66f76170b40dcf1df5bfe38a12da9a8
colewortman <colewortman@gmail.com>
2025-03-19 00:31:13 -0500

    Added an exit function and a command registry

--- main.go
+++ main.go
@@ -6,6 +6,20 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name		string
+	description string
+	callback	func() error
+}
+
+var commandRegistry = map[string]cliCommand{
+    "exit": {
+        name:        "exit",
+        description: "Exit the Pokedex",
+        callback:    commandExit,
+    },
+}
+
 func cleanInput(text string) []string {
 	lowercase := strings.ToLower(text)
 	words := strings.Fields(lowercase)
@@ -13,12 +27,32 @@ func cleanInput(text string) []string {
 	return words
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
+
 		if scanner.Scan() {
-			fmt.Println("Your command was:", cleanInput(scanner.Text())[0])
+			args := cleanInput(scanner.Text())
+			if len(args) == 0 {
+				continue
+			}
+
+			command := args[0]
+			
+			if cmd, found := commandRegistry[command]; found {
+				if err := cmd.callback(); err != nil {
+					fmt.Println("Error:", err)
+				}
+			} else {
+				fmt.Println("Unknown command")
+			}
 		}
 	}
 }
